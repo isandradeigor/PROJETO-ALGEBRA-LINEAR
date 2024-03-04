708f0d246d8eaca048d877dc24f212493c5a45f0
João Igor <joaoigor21052004@gmail.com>
2024-03-03 21:03:34 -0300

    OPERACOES 1 MATRIX E VETOR

--- src/App.java
+++ src/App.java
@@ -10,7 +10,7 @@ public class App {
         char choiceType = scanner.next().charAt(0);
         if (choiceType == 'M' || choiceType == 'm') {
             // Operações com Matriz
-            System.out.println("Digite 'T' para transpor ou 'S' para somar:");
+            System.out.println("Digite 'T' para transpor, 'S' para somar, ou 'N' para não fazer operação:");
             char choiceOperation = scanner.next().charAt(0);
 
             if (choiceOperation == 'T' || choiceOperation == 't') {
@@ -55,12 +55,24 @@ public class App {
                 Matrix.apresentarMatriz(matrix2);// apresenta a segunda matriz
                 System.out.println("Resultado da soma das matrizes:");
                 Matrix.apresentarMatriz(matrizSoma);
+            } else if (choiceOperation == 'N' || choiceOperation == 'n') {
+                // Nenhuma operação, apenas exibir a matriz
+                System.out.println("Digite o número de linhas e colunas da matriz:");
+                int rows = scanner.nextInt();
+                int cols = scanner.nextInt();
+                double[][] data = new double[rows][cols];
+
+                // CHAMAR MATRIZ
+                Matrix matrix = new Matrix(rows, cols, data);
+                Matrix.inserirMatriz(rows, cols, data);
+                System.out.println("Matriz:");
+                Matrix.apresentarMatriz(matrix);
             } else {
-                System.out.println("Operação inválida. Por favor, digite 'T' para transpor ou 'S' para somar.");
+                System.out.println("Operação inválida. Por favor, digite 'T' para transpor, 'S' para somar, ou 'N' para não fazer operação.");
             }
-        } else if (choiceType == 'V' || choiceType == 'v') {
+        }else if (choiceType == 'V' || choiceType == 'v') {
             // Operações com Vetor
-            System.out.println("Digite 'T' para transpor:");
+            System.out.println("Digite 'T' para transpor, 'S' para somar, ou 'N' para não fazer operação:");
             char choiceOperation = scanner.next().charAt(0);
 
             if (choiceOperation == 'T' || choiceOperation == 't') {
@@ -79,8 +91,44 @@ public class App {
                 vector.apresentarVectorLinha(dim, vetorData);
                 System.out.println("Vetor transposto:");
                 vetorTransposto.apresentarVectorColuna(dim, vetorTransposto.elements);
+            } else if (choiceOperation == 'S' || choiceOperation == 's') {
+                // Soma de Vetores
+                System.out.println("Digite o tamanho do primeiro vetor:");
+                int dim1 = scanner.nextInt();
+                double[] vetorData1 = new double[dim1];
+
+                // CHAMAR PRIMEIRO VETOR
+                Vector vector1 = new Vector(dim1, vetorData1);
+                vector1.inserirVector(dim1, vetorData1);
+                System.out.println("Digite o tamanho do segundo vetor:");
+                int dim2 = scanner.nextInt();
+                double[] vetorData2 = new double[dim2];
+
+                // CHAMAR SEGUNDO VETOR
+                Vector vector2 = new Vector(dim2, vetorData2);
+                vector2.inserirVector(dim2, vetorData2);
+
+                // Somar os dois vetores e imprimir o resultado
+                Vector vetorSoma = LinearAlgebra.sum(vector1, vector2);
+                System.out.println("Primeiro vetor:");
+                vector1.apresentarVectorLinha(dim1, vetorData1);
+                System.out.println("Segundo vetor:");
+                vector2.apresentarVectorLinha(dim2, vetorData2);
+                System.out.println("Resultado da soma dos vetores:");
+                vetorSoma.apresentarVectorLinha(vetorSoma.dim, vetorSoma.elements);
+            } else if (choiceOperation == 'N' || choiceOperation == 'n') {
+                // Nenhuma operação, apenas exibir o vetor
+                System.out.println("Digite o tamanho do vetor:");
+                int dim = scanner.nextInt();
+                double[] vetorData = new double[dim];
+
+                // CHAMAR VETOR
+                Vector vector = new Vector(dim, vetorData);
+                vector.inserirVector(dim, vetorData);
+                System.out.println("Vetor:");
+                vector.apresentarVectorLinha(dim, vetorData);
             } else {
-                System.out.println("Operação inválida. Por favor, digite 'T' para transpor.");
+                System.out.println("Operação inválida. Por favor, digite 'T' para transpor, 'S' para somar, ou 'N' para não fazer operação.");
             }
         } else {
             System.out.println("Escolha inválida. Por favor, digite 'M' para matriz ou 'V' para vetor.");
